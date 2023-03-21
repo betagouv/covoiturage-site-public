563789fc610368419f07c291f0e72bd2b4a93053
Ludovic Delhomme <ludovic.delhomme@beta.gouv.fr>
2023-03-21 19:01:54 +0100

    test md and map component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
+import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
 });
---
+++ src/pages/test.md
@@ -0,0 +1,7 @@
+---
+layout: '@/layouts/PostLayout.astro'
+title: 'My Markdown page'
+---
+
+# sous-titre
+This is my page, written in **Markdown.**
