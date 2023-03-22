2eaa8276031df4c722d2f138fece6d6781b67db7
Ludovic Delhomme <ludovic.delhomme@beta.gouv.fr>
2023-03-22 11:14:54 +0100

    debug map build

--- src/components/observatoire/Map.vue
+++ src/components/observatoire/Map.vue
@@ -1,53 +1,45 @@
 <template>
   <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center">
     <div class="fr-col">
-        <div class="fr-callout">
-          <h3 class="fr-callout__title ">{{props.title}}</h3>
-          <p class="fr-callout__text">
-            <div id="map"></div>
-          </p>
-        </div>
+      <div class="fr-callout">
+        <h3 class="fr-callout__title">{{ props.title }}</h3>
+        <div id="map"></div>
       </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 
-import { Map, NavigationControl, AttributionControl } from 'maplibre-gl'
+import maplibregl from 'maplibre-gl'
+import type { Map } from 'maplibre-gl'
+import { onMounted } from 'vue'
 
-
-export default {
-  props: {
-    title: String
-  },
-  setup(props){
-    function createMap() {
-      const map =  new Map({
-        container: 'map',
-        style: 'https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json',
-        center: [4.5, 45.6],
-        zoom: 7,
-        attributionControl: false,
-      })
-      map.scrollZoom.disable()
-      map.addControl(new NavigationControl({}), 'top-right')
-      map.addControl(new AttributionControl({
-          compact: true
-      }))
-    }
-    return { props, createMap }
-  },
-  mounted(){
-    this.createMap()
-  }
-  
+type Props = {
+  title: String
 }
+const props = defineProps<Props>()
 
+let map:Map
 
+onMounted(() => {
+  createMap()
+})
 
-
-
-
+function createMap() {
+    map = new maplibregl.Map({
+      container: 'map',
+      style: 'https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json',
+      center: [4.5, 45.6],
+      zoom: 7,
+      attributionControl: false,
+    })
+    map.scrollZoom.disable()
+    map.addControl(new maplibregl.NavigationControl({}), 'top-right')
+    map.addControl(new maplibregl.AttributionControl({
+        compact: true
+    }))
+  }
 </script>
 
 <style lang="scss">
